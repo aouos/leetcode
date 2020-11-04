f2a9b40ce31f23f17d48b216433c30d21baeb520
aouos <aouos@qq.com>
2020-11-04 21:53:23 +0800

    0035 update

--- 0035_searchInsert.js
+++ 0035_searchInsert.js
@@ -4,9 +4,16 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-  let l = 0;
-  while (target > nums[l]) {
-    l++;
+  const len = nums.length;
+  let l = 0, r = len - 1, res = len;
+  while (l <= r) {
+    let mid = ((r - l) >> 1) + l;
+    if (target <= nums[mid]) {
+      res = mid;
+      r = mid - 1;
+    }else {
+      l = mid + 1;
+    }
   }
-  return l;
+  return res;
 };
