0ecb1f807d95f194d983090df2be0a6ed50fb290
aouos <aouos@qq.com>
2020-11-11 09:21:23 +0800

    0031_nextPermutation

--- 0031_nextPermutation.js
+++ 0031_nextPermutation.js
@@ -2,42 +2,24 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-let nums = [3,2,1];
 var nextPermutation = function (nums) {
-  function upperaArr(arr, idx) {
-    let preArr = arr.slice(0, idx);
-    let nextArr = arr.slice(idx);
-    for(let i = 0; i < nextArr.length - 1; i++) {
-      for(let j = i + 1; j < nextArr.length; j++) {
-        if(nextArr[i] > nextArr[j]) {
-          let temp = nextArr[i];
-          nextArr[i] = nextArr[j];
-          nextArr[j] = temp;
-        }
-      }
-    }
-    for(let k = 0; k < nextArr.length; k++) {
-      preArr.push(nextArr[k])
-    }
-    return preArr;
-  }
-  let j = nums.length - 1,i = j;
-  while (j > 0) {
-    if (nums[j] > nums[j - 1]) {
-      break;
-    }
-    j--;
-  }
-  while (i >= j) {
-    if (nums[i] > nums[j - 1]) {
-      let temp = nums[i];
-      nums[i] = nums[j - 1];
-      nums[j - 1] = temp;
-      break;
-    }
+  let i = nums.length - 2;
+  while (i >= 0 && nums[i] >= nums[i + 1]) {
     i--;
   }
-  let res = upperaArr(nums, j);
-  return res;
+  if (i >= 0) {
+    let j = nums.length - 1;
+    while (j >= 0 && nums[j] <= nums[i]) {
+      j--;
+    }
+    [nums[i], nums[j]] = [nums[j], nums[i]];
+  }
+  let l = i + 1;
+  let r = nums.length - 1;
+  while (l < r) {
+    [nums[l], nums[r]] = [nums[r], nums[l]];
+    l++;
+    r--;
+  }
+  return nums;
 };
-console.log(nextPermutation(nums));
