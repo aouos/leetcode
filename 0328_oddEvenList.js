2fe2401975b795de712314e3cdd47ce9aaec6c08
aouos <aouos@qq.com>
2020-11-13 09:47:22 +0800

    0328_oddEvenList

---
+++ 0328_oddEvenList.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+  if(head === null) {
+    return head;
+  }
+  let odd = head, even = head.next, evenHead = even;
+  while (odd !== null && even.next !== null) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = evenHead;
+  return head;
+};
