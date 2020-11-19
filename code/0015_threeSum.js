b74ec1086e342473ba25930440539035503bc097
aouos <aouos@qq.com>
2020-11-19 22:33:36 +0800

    threeSum review

--- code/0015_threeSum.js
+++ code/0015_threeSum.js
@@ -2,20 +2,30 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+const nums = [-1, 0, 1, 1, 2, 2, -1, -4];
+
 var threeSum = function (nums) {
   let res = [];
-  nums.sort((a, b) => { return a - b });
+  // 数组长度小于3直接返回
   if (nums.length < 3) {
     return res;
   }
+  // 对数组进行升序排列
+  nums.sort((a, b) => { return a - b });
+  // 遍历数组,到长度减去2位结束
   for (let i = 0; i < nums.length - 2; i++) {
+    // 如果当前值大于0直接结束
     if (nums[i] > 0) {
       break;
     }
+    // 当前值与前一个相等跳过
     if (nums[i] === nums[i - 1]) {
       continue;
     }
+    // 从当前值右边开始定义双指针从两头进行查找
     for (let x = i + 1, y = nums.length - 1; x < y;) {
+      // 三个数和小于0,x右移动;三个数和大于0,y左移动
       if (nums[i] + nums[x] + nums[y] < 0) {
         x++;
       } else if (nums[i] + nums[x] + nums[y] > 0) {
@@ -32,3 +42,5 @@ var threeSum = function (nums) {
   }
   return res;
 };
+
+console.log(threeSum(nums));
