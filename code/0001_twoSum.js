f1ef6bf165817ee69e3d599591d2a2c385bc7921
aouos <aouos@qq.com>
2020-11-19 20:58:11 +0800

    twoSum review

--- code/0001_twoSum.js
+++ code/0001_twoSum.js
@@ -3,13 +3,23 @@
  * @param {number} target
  * @return {number[]}
  */
+
+const nums = [2, 11, 15, 7], target = 9;
+
 var twoSum = function (nums, target) {
+  // 构建一个哈希表
   const map = new Map();
+  // 遍历数组nums
   for (let i = 0; i < nums.length; i++) {
-    const diff = target - nums[i];
+    // 得到当前值与target的差，在map中进行查找
+    let diff = target - nums[i];
     if (map.has(diff)) {
+      // 找到值后返回坐标
       return [map.get(diff), i];
     }
+    // 将当前值的值与对应索引存入map
     map.set(nums[i], i);
   }
 };
+
+console.log(twoSum(nums, target));
