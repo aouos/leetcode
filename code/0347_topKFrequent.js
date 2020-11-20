73b10b71959a4d671b840df1c0c7b64f2768580f
aouos <aouos@qq.com>
2020-11-20 14:50:43 +0800

    topKFrequent second

--- code/0347_topKFrequent.js
+++ code/0347_topKFrequent.js
@@ -7,14 +7,14 @@
 const nums = [1, 1, 3, 3, 3, 2, 4, 1, 2, 2, 3, 3], k = 2;
 
 var topKFrequent = function (nums, k) {
+  let ans = [];
   // 创建一个哈希表
   let map = new Map();
-  // 遍历数组存入值和计数
-  for (let idx in nums) {
-    map[nums[idx]] = map[nums[idx]] ? map[nums[idx]] + 1 : 1;
-  }
-  // 遍历map进行排序
-  return Object.keys(map).sort((a, b) => map[b] - map[a]).slice(0, k);
+  nums.map(num => {
+    map.set(num, map.has(num) ? map.get(num) + 1 : 1);
+  })
+  Array.from(map).sort((pre,next) => next[1] - pre[1]).slice(0,k).map(a => ans.push(a[0]));
+  return ans;
 };
 
 console.log(topKFrequent(nums, k));
