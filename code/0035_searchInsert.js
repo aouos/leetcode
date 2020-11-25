09a9e04b8b0ba084e804437808b67c21423634c5
aouos <aouos@qq.com>
2020-11-25 21:05:02 +0800

    0035_searchInsert review

--- code/0035_searchInsert.js
+++ code/0035_searchInsert.js
@@ -3,17 +3,24 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
-  const len = nums.length;
-  let l = 0, r = len - 1, res = len;
+
+const nums = [1, 3, 5, 6, 7, 8], target = 0;
+
+var searchInsert = function (nums, target) {
+  // 定义指,l指向最左端,r指向最右端
+  let l = 0, r = nums.length - 1, ans = nums.length;
+  // 当l<=r时执行循环
   while (l <= r) {
+    // 中间下标使用位运算进行求值
     let mid = ((r - l) >> 1) + l;
-    if (target <= nums[mid]) {
-      res = mid;
-      r = mid - 1;
-    }else {
+    if (target > nums[mid]) {
       l = mid + 1;
+    } else {
+      ans = mid;
+      r = mid - 1;
     }
   }
-  return res;
+  return ans;
 };
+
+console.log(searchInsert(nums, target));
